562e4b2d4c2f45d94b1ef8198e646de124da2e09
patbrakesch <patricia.branschaedel@kesch.com>
2026-06-30 11:06:26 +0200

    migros V1

---
+++ app/migros/page migros.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { useState } from "react";
+import { CheckCircle } from "lucide-react";
+import "./globals.css";
+
+export default function Home() {
+  const [sent, setSent] = useState(false);
+
+  async function submitForm(e: any) {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    const data = Object.fromEntries(form.entries());
+
+    const res = await fetch("/api/submit", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+
+  if (res.ok) {
+  setSent(true);
+  } else {
+  const result = await res.json();
+
+    if (
+      result.code === "23505" ||
+      result.error?.toLowerCase().includes("duplicate") ||
+      result.error?.toLowerCase().includes("unique")
+    ) {
+      alert("Du hast bereits am Gewinnspiel teilgenommen. Pro Person ist nur eine Teilnahme möglich.");
+    } else {
+      alert("Es gab ein Problem. Bitte versuche es nochmals.");
+    }
+  }
+    
+  }
+
+  if (sent) {
+    return (
+      <main className="page thankYouPage">
+        <div className="thankYouCard">
+          <div className="checkmark">✓</div>
+          <h1>Danke!</h1>
+          <p>Du nimmst jetzt an der Verlosung teil.</p>
+          <p>Viel Glück!</p>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <main className="page">
+     <section className="heroImage">
+      <img src="/hero2.png" alt="Gewinnspiel Openair Frauenfeld" />
+    </section>
+
+      <section className="formCard">
+        <form onSubmit={submitForm}>
+          <input name="first_name" placeholder="Vorname" required />
+          <input name="last_name" placeholder="Nachname" required />
+          <input name="birthdate" type="date" required />
+          <input name="address" placeholder="Adresse" required />
+          <input name="zip" placeholder="PLZ" required />
+          <input name="city" placeholder="Ort" required />
+          <input name="email" type="email" placeholder="E-Mail" required />
+          <input name="phone" placeholder="Telefonnummer" required />
+
+          <label className="check">
+            <input type="checkbox" required />
+            <span>Ich akzeptiere die Teilnahmebedingungen.</span>
+          </label>
+
+          <label className="check">
+            <input type="checkbox" required />
+            <span>Ich akzeptiere die Datenschutzerklärung.</span>
+          </label>
+
+          <button type="submit">TEILNEHMEN</button>
+        </form>
+      </section>
+
+      <footer>Teilnahme bis: xx.xx.2026 · Datenschutz · Teilnahmebedingungen</footer>
+    </main>
+  );
+}
